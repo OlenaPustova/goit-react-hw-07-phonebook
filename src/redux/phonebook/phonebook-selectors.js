eb834ad7bd05b255535eb5e12e2814fb79486e75
OlenaPustova <i.am.elena.p@gmail.com>
2022-05-24 17:41:53 +0300

    getFilteredContacts move to selectors

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,3 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getFilterSelector = state => state.contacts.filter;
 export const getContactsSelector = state => state.contacts.items;
 export const getLoadingSelector = state => state.isLoading;
+
+export const getFilteredContacts = createSelector(
+  [getFilterSelector, getContactsSelector],
+  (filter, contacts) => {
+    const filterToLowerCase = filter.toLowerCase();
+    console.log('getfiltered');
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterToLowerCase)
+    );
+  }
+);
